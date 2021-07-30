1e64fdf84c49f144ea6350ac7c8f422354f79c0c
minh.vu <minh.vu@sotatek.com>
2021-07-30 16:34:59 +0700

    change color

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Pace Art App123 - version-9!!</h1> \n');
+  res.send('<h1 style="color:DodgerBlue;">Pace Art App ok - version-9!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
